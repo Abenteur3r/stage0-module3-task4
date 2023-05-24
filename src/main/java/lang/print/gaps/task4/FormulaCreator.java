8996552770c82180ff173498947ffd578aca2d2e
Ayan <ajan.kohan@gmail.com>
2023-05-25 02:55:15 +0600

    M3 task4

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        double a=2.1, b=3.2;
+        double formula = ((9*Math.pow(a,2) - 5*b + 2 + a - 7) * ((a + b - 4*a*b) / 2));
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 'c';
+        char h = 'h';
+        char a = 'a';
+        char r = 'r';
+        int cChar = c;
+        int hChar = h;
+        int aChar = a;
+        int rChar = r;
+        System.out.println(cChar);
+        System.out.println(hChar);
+        System.out.println(aChar);
+        System.out.println(rChar);
     }
 }
